d9243a3972f344951309342da7a30ad0219d59eb
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-11-29 18:48:30 +0530

    Changes Made By Combining ADD and UPDATE functionality

--- src/myquery.js
+++ src/myquery.js
@@ -1,62 +1,41 @@
-"use strict"
-var arr = []; //Product Array
-$("#update_product").hide(); //Make Update Button Hide by default
-$("#success").hide(); //Make Success Notification hide by default
-$("#error").hide(); //Make Error Notification hide by default
+var prodArr = []; //Product Array
+document.getElementById("success").style.display = "none"; //Hide Success Notification
+document.getElementById("error").style.display = "none"; //Hide Error Notification
 
 $(document).ready(function () {
+  //jQuery Ready Function
   $("#add_product").click(function () {
     var product_sku = $("#product_sku").val();
     var product_name = $("#product_name").val();
     var product_price = $("#product_price").val();
     var product_quantity = $("#product_quantity").val();
 
-    var obj = {                          //object to store the data
+    var obj = {
+      //Making object for storing all input values
       SKU: product_sku,
       Name: product_name,
       Price: product_price,
       Quantity: product_quantity,
     };
-    if                                 //Validation for all the input fields
-      (obj.SKU == "" && obj.Name == "" && obj.Price == "" && obj.Quantity == "") {
-      $("#error").text("All field are empty.");
-      $("#error").show();
-    } else if (obj.SKU == "") {
-      $("#error").text("SKU field is empty.");
-      $("#error").show();
-    } else if (obj.Name == "") {
-      $("#error").text("Name field is empty.");
-      $("#error").show();
-    } else if (obj.Price == "") {
-      $("#error").text("Price field is empty.");
-      $("#error").show();
-    } else if (obj.Quantity == "") {
-      $("#error").text("Quantity field is empty.");
-      $("#error").show();
-    } else if (isNaN(obj.SKU)) {
-      $("#error").text("SKU field must have an integer value");
-      $("#error").show();
-    } else if (!isNaN(obj.Name)) {
-      $("#error").text("Name field should be string");
-      $("#error").show();
-    } else if (isNaN(obj.Price)) {
-      $("#error").text("Price field should be integer");
-      $("#error").show();
-    } else if (isNaN(obj.Quantity)) {
-      $("#error").text("Quantity field should be integer");
-      $("#error").show();
-    } else {
-      arr.push(obj);
-      display();
-    }
+    errorDisplay(obj);
+  });
+
+  //function for hide notification
+  $("#close-success").click(function () {
+    $("#success").hide();
+  });
+  $("#close-error").click(function () {
+    $("#error").hide();
   });
 });
+
+//Display function of table
 function display() {
-  $("#success").show(); //When the data is push into the array successfully Success notification become visible
+  $("#success").show();
   $("#error").hide();
   var table =
     "<table><tr><th>SKU</th><th>Name</th><th>Price</th><th>Quantity</th><th>Action</th></tr>";
-  arr.forEach((element) => {
+  prodArr.forEach((element) => {
     table +=
       '<tr id="row"><td>' +
       element.SKU +
@@ -73,75 +52,81 @@ function display() {
       "')\">Delete</a></td></tr>";
   });
   table += "</table>";
-  $("#product_list").html(table);
+  document.getElementById("product_list").innerHTML = table;
 }
-function edit(val) {    //Function to edit the entere values
-  $("#add_product").hide(); // Add product button will get hide
-  $("#update_product").show();//Now update button will be visible
-
-  for (let i = 0; i < arr.length; i++) {
-    if (val == arr[i].SKU) {
-      $("#product_sku").val(arr[i].SKU);
-      $("#product_name").val(arr[i].Name);
-      $("#product_price").val(arr[i].Price);
-      $("#product_quantity").val(arr[i].Quantity);
-      arr.splice(i, 1);
+//Edit function
+function edit(val) {
+  $("#add_product").prop("value", "UPDATE");
+  for (let i = 0; i < prodArr.length; i++) {
+    if (val == prodArr[i].SKU) {
+      document.getElementById("product_sku").value = prodArr[i].SKU;
+      document.getElementById("product_name").value = prodArr[i].Name;
+      document.getElementById("product_price").value = prodArr[i].Price;
+      document.getElementById("product_quantity").value = prodArr[i].Quantity;
+      prodArr.splice(i, 1);
     }
   }
   display();
 }
-function update() {    //Update function to update the data in the fields
-  $("#update_product").hide();
-  $("#add_product").show();
 
-  var product_sku = $("#product_sku").val();
-  var product_name = $("#product_name").val();
-  var product_price = $("#product_price").val();
-  var product_quantity = $("#product_quantity").val();
-  var obj = {
-    SKU: product_sku,
-    Name: product_name,
-    Price: product_price,
-    Quantity: product_quantity,
-  };
-
-  if (obj.SKU == "" && obj.Name == "" && obj.Price == "" && obj.Quantity == ""){
-  $("#error").text("All field are empty.");
-  $("#error").show();
-} else if (obj.SKU == "") {
-  $("#error").text("SKU field is empty.");
-  $("#error").show();
-} else if (obj.Name == "") {
-  $("#error").text("Name field is empty.");
-  $("#error").show();
-} else if (obj.Price == "") {
-  $("#error").text("Price field is empty.");
-  $("#error").show();
-} else if (obj.Quantity == "") {
-  $("#error").text("Quantity field is empty.");
-  $("#error").show();
-} else if (isNaN(obj.SKU)) {
-  $("#error").text("SKU field must have an integer value");
-  $("#error").show();
-} else if (!isNaN(obj.Name)) {
-  $("#error").text("Name field should be string");
-  $("#error").show();
-} else if (isNaN(obj.Price)) {
-  $("#error").text("Price field should be integer");
-  $("#error").show();
-} else if (isNaN(obj.Quantity)) {
-  $("#error").text("Quantity field should be integer");
-  $("#error").show();
-} else {
-  arr.push(obj);
-  display();
-  } 
+function errorDisplay(obj) {
+  if (
+    //Implemention the all check requried
+    obj.SKU == "" &&
+    obj.Name == "" &&
+    obj.Price == "" &&
+    obj.Quantity == ""
+  ) {
+    $("#error").text("All field are empty.");
+    $("#success").hide();
+    $("#error").show();
+  } else if (obj.SKU == "") {
+    $("#error").text("SKU field is empty.");
+    $("#success").hide();
+    $("#error").show();
+  } else if (obj.Name == "") {
+    $("#error").text("Name field is empty.");
+    $("#success").hide();
+    $("#error").show();
+  } else if (obj.Price == "") {
+    $("#error").text("Price field is empty.");
+    $("#success").hide();
+    $("#error").show();
+  } else if (obj.Quantity == "") {
+    $("#error").text("Quantity field is empty.");
+    $("#success").hide();
+    $("#error").show();
+  } else if (isNaN(obj.SKU)) {
+    $("#error").text("SKU field should be integer");
+    $("#success").hide();
+    $("#error").show();
+  } else if (!isNaN(obj.Name)) {
+    $("#error").text("Name field should be string");
+    $("#success").hide();
+    $("#error").show();
+  } else if (isNaN(obj.Price)) {
+    $("#error").text("Price field should be integer");
+    $("#success").hide();
+    $("#error").show();
+  } else if (isNaN(obj.Quantity)) {
+    $("#error").text("Quantity field should be integer");
+    $("#success").hide();
+    $("#error").show();
+  } else {
+    $("#add_product").prop("value", "ADD PRODUCT");
+    prodArr.push(obj);
+    console.log(prodArr);
+    display();
+  }
 }
-function delProd(val) {  //Delete function to delete the product from the list
-  for (let i = 0; i < arr.length; i++) {
-    if (val == arr[i].SKU) {
-      arr.splice(i, 1);
+
+//Function for delete the item
+function delProd(val) {
+  for (let i = 0; i < prodArr.length; i++) {
+    if (val == prodArr[i].SKU) {
+      prodArr.splice(i, 1);
     }
   }
   display();
 }
+
